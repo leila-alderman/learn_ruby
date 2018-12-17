2ac72d7de9f0403bc32c3ba7a9731c1b7c81f36d
leila-alderman <leila.alderman@gmail.com>
2018-12-17 15:28:45 -0500

    Solve timer exercsie

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,13 @@
 class Timer
   #write your code here
+  attr_accessor :seconds
+  def initialize
+    @seconds = 0
+  end
+  
+  def time_string
+    t1 = Time.new(2010, 1, 1, 0, 0, 0)
+    t2 = t1 + @seconds
+    t2.strftime("%T")
+  end
 end
