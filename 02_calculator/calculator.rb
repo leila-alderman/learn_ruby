4a1c5cff577cf54afce6691bbbb62db284338a16
leila-alderman <leila.alderman@gmail.com>
2018-12-17 13:50:58 -0500

    Solve calculator exercise

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,44 @@
 #write your code here
+def add(num1, num2)
+    num1 + num2
+end 
+
+def subtract(num1, num2)
+    num1 - num2
+end 
+
+# Function that takes an array of numbers and calculates the total
+def sum(arr)
+    total = 0
+    arr.each { |x|
+    total += x
+    }
+    return total
+end 
+
+#Function that multiplies a variable number of given numbers
+def multiply(*nums)
+    total = 1
+    nums.each { |x| 
+    total *= x
+    }
+    return total
+end 
+
+# Function that calculates an exponent
+def power(num1, num2)
+    num1 ** num2
+end 
+
+# Function that calculates the factorial of a given number
+def factorial(num)
+    if num == 1 || num == 0
+        return 1
+    else
+        total = 1
+        for i in 1..num
+            total *= i
+        end 
+        return total
+    end 
+end 
