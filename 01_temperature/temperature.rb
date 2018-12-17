7c3cc17efaea1468b169048b31e475a3fdcaa14d
leila-alderman <leila.alderman@gmail.com>
2018-12-17 13:33:49 -0500

    Solve temperature exercise

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,11 @@
 #write your code here
+
+# Function to convert temperature from F to C
+def ftoc(tempF)
+    (tempF - 32.0) * (5.0 / 9.0)
+end 
+
+# Function to convert temperature from C to F
+def ctof(tempC)
+    (tempC * (9.0/5.0)) + 32
+end 
