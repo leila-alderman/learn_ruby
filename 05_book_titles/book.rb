93e531acf70333aa5d11d1f11c00fea6acf5a49c
leila-alderman <leila.alderman@gmail.com>
2018-12-17 15:01:31 -0500

    Solve book titles exercise

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,20 @@
 class Book
 # write your code here
+    attr_reader :title
+
+    def initialize; end
+
+    def title=(name)
+        uncaps = ['a', 'an', 'the', 'and', 'in', 'of']
+        words = name.split(" ").map do |word|
+            if uncaps.include?(word)
+                word
+            else 
+                word.capitalize
+            end
+        end
+        words[0].capitalize!
+        @title = words.join(" ")
+    end 
+
 end
